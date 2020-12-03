4532e2a2bc4b0c2fd805b0a22a4a593095b80252
kensuke-b <75057491+kensuke-b@users.noreply.github.com>
2020-12-03 17:47:02 +0900

    顔枠がグレースケールまでできました

--- group6_11/imagecv.py
+++ group6_11/imagecv.py
@@ -1,26 +1,33 @@
 import cv2
+import numpy as np
 
+# img=cv2.imread('group6_11/sample.jpg',cv2.IMREAD_GRAYSCALE)
 
+# cv2.imshow('result',img)
+# cv2.waitKey(0)
+# cv2.destroyAllWindows()
 
 def facewaku(img1):
-    cascade_path =  "./cascades/haarcascade_frontalface_default.xml"
-    cascade = cv2.CascadeClassifier(cascade_path)
+    #cascade_path =  "./cascades/haarcascade_frontalface_default.xml"
+    #cascade = cv2.CascadeClassifier(cascade_path)
 
     color = (255, 255, 255) #検出した顔を囲む四角形の色
 
-    #画像ファイルの読み込み
-    img = cv2.imread(img1)
+    #grayscale
+    img_g=cv2.cvtColor(img1,cv2.COLOR_BGR2GRAY)
 
-    #グレースケールに変換する
-    img_g = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+
+
  
     
-    #rect = cascade.detectMultiScale(img_g)
-    #if len(rect) > 0:
-     #   for x, y, w, h in rect:
-      #      cv2.rectangle(img, (x, y), (x+w, y+h), color)
+    # rect = cascade.detectMultiScale(img)
+    # if len(rect) > 0:
+    #     for x, y, w, h in rect:
+    #         cv2.rectangle(img, (x, y), (x+w, y+h), color)
 
-    cv2.imshow('detected', img)
+   
+    cv2.imshow('result', img_g)
     cv2.waitKey(0)
     cv2.destroyAllWindows()
 
@@ -33,6 +40,6 @@ def facewaku(img1):
 
 if __name__ == "__main__":
  #この辺にwatchdog
- img1 = cv2.imread('sample.png')
+ img1 = cv2.imread('group6_11/sample.jpg')
  
  facewaku(img1)
