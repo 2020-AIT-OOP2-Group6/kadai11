7f11dd1a6390b31e74d8181c1d81d5f5c9f7fa26
fuga-h <k19094kk@aitech.ac.jp>
2020-12-03 15:33:58 +0900

    web.pyインポート

---
+++ group6_11/web.py
@@ -0,0 +1,9 @@
+import os
+# request フォームから送信した情報を扱うためのモジュール
+# redirect  ページの移動
+# url_for アドレス遷移
+from flask import Flask, request, redirect, url_for
+# ファイル名をチェックする関数
+from werkzeug.utils import secure_filename
+# 画像のダウンロード
+from flask import send_from_directory
