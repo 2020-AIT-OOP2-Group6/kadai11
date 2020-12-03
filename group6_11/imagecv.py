9f212d4cc49d112229de11d72f44656afaa4c1d1
kensuke-b <75057491+kensuke-b@users.noreply.github.com>
2020-12-03 19:07:24 +0900

    顔検出完了

--- group6_11/imagecv.py
+++ group6_11/imagecv.py
@@ -1,6 +1,7 @@
 
 import cv2
 import numpy as np
+import os
 
 # img=cv2.imread('group6_11/sample.jpg',cv2.IMREAD_GRAYSCALE)
 
@@ -9,23 +10,35 @@ import numpy as np
 # cv2.destroyAllWindows()
 
 def facewaku(img1):
-    #cascade_path =  "./cascades/haarcascade_frontalface_default.xml"
-    #cascade = cv2.CascadeClassifier(cascade_path)
-
-    color = (255, 255, 255) #検出した顔を囲む四角形の色
-
-    #grayscale
-    img_g=cv2.cvtColor(img1,cv2.COLOR_BGR2GRAY)
+    cascade_path =  "group6_11/cascades/haarcascade_frontalface_default.xml"
     
-    # rect = cascade.detectMultiScale(img)
-    # if len(rect) > 0:
-    #     for x, y, w, h in rect:
-    #         cv2.rectangle(img, (x, y), (x+w, y+h), color)
+    cascade = cv2.CascadeClassifier(cascade_path)
 
-   
-    cv2.imshow('result', img_g)
+    img=img1
+    #grayscale
+    img_g=cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+
+    assert os.path.isfile(cascade_path), 'haarcascade_frontalface_default.xml がない'
+    
+    
+    facerect = cascade.detectMultiScale(img_g, scaleFactor=1.1, minNeighbors=2, minSize=(30, 30))
+
+
+    print(facerect)
+    color = (255, 255, 255) #白
+
+    # 検出した場合
+    if len(facerect) > 0:
+
+    #検出した顔を囲む矩形の作成
+        for rect in facerect:
+            cv2.rectangle(img, tuple(rect[0:2]),tuple(rect[0:2]+rect[2:4]), color, thickness=2)
+ 
+    cv2.imwrite('group6_11/facewaku/result.jpg', img)
+    #cv2.imwrite('result', img)
     cv2.waitKey(0)
     cv2.destroyAllWindows()
+
 
 
 #画像の保存
@@ -36,6 +49,6 @@ def facewaku(img1):
 
 if __name__ == "__main__":
  #この辺にwatchdog
- img1 = cv2.imread('group6_11/sample.jpg')
+ img1 = cv2.imread('group6_11/sample2.jpg')
  
  facewaku(img1)
