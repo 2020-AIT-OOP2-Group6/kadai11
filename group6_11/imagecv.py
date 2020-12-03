3dcecc4eddd92ea5ae96f8da33a6638fdb578317
k19081 <k19081kk@aitech.ac.jp>
2020-12-03 16:47:26 +0900

    またエラー出たので直しました

--- group6_11/imagecv.py
+++ group6_11/imagecv.py
@@ -3,8 +3,8 @@ import cv2
 
 def grayscale(self, img1):
  #グレースケール化
- img_gray = cv2.cvtColor(image-name, cv2.COLOR_BGR2GRAY)
- cv2.imwrite(grayscale-img, img_gray)
+ self.img_gray = cv2.cvtColor(img1, cv2.COLOR_BGR2GRAY)
+ cv2.imwrite(grayscale-img, self.img_gray)
 
 
 
