caa1a9997abae137d3169b9ce8d9d98d13ff435c
kensuke-b <75057491+kensuke-b@users.noreply.github.com>
2020-12-08 11:18:32 +0900

    Update imagecv.py

--- group6_11/imagecv.py
+++ group6_11/imagecv.py
@@ -2,12 +2,14 @@
 import cv2
 import numpy as np
 import os
+import time
+from watchdog.events import FileSystemEventHandler
+from watchdog.observers import Observer
 
-# img=cv2.imread('group6_11/sample.jpg',cv2.IMREAD_GRAYSCALE)
+target_dir = "group6_11/input-image"
 
-# cv2.imshow('result',img)
-# cv2.waitKey(0)
-# cv2.destroyAllWindows()
+
+
 
 def facewaku(img1):
     cascade_path =  "group6_11/cascades/haarcascade_frontalface_default.xml"
@@ -48,9 +50,50 @@ def facewaku(img1):
 #cv2.imwrite(f"./facewaku/face_rectangle{i}.jpg", img)
 #i=i+1
 
+# FileSystemEventHandler の継承クラスを作成
+class FileChangeHandler(FileSystemEventHandler):
+     # ファイル作成時のイベント
+     def on_created(self, event):
+         filepath = event.src_path
+         filename = os.path.basename(filepath)
+         print('%s created' % filename)
+         image_path="group6_11/input-image/" + filename
+         img1=cv2.imread("image_path")
+         facewaku(img1)
+
+     # ファイル変更時のイベント
+     def on_modified(self, event):
+         filepath = event.src_path
+         filename = os.path.basename(filepath)
+         print('%s changed' % filename)
+
+     # ファイル削除時のイベント
+     def on_deleted(self, event):
+         filepath = event.src_path
+         filename = os.path.basename(filepath)
+         print('%s deleted' % filename)
+
+     # ファイル移動時のイベント
+     def on_moved(self, event):
+         filepath = event.src_path
+         filename = os.path.basename(filepath)
+         print('%s moved' % filename)
+
 
 if __name__ == "__main__":
  #この辺にwatchdog
- img1 = cv2.imread('group6_11/sample2.jpg')
+ #img1 = cv2.imread('group6_11/sample2.jpg')
  
- facewaku(img1)
+ #facewaku(img1)
+ # ファイル監視の開始
+     event_handler = FileChangeHandler()
+     observer = Observer()
+     observer.schedule(event_handler, target_dir, recursive=True)
+     observer.start()
+     # 処理が終了しないようスリープを挟んで無限ループ
+     try:
+         while True:
+             time.sleep(0.1)
+     except KeyboardInterrupt:
+         observer.stop()
+     observer.join()
