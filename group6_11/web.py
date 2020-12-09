a7b002e287d59cb33a8fcfcffd0ae1eaad3ac953
fuga-h <k19094kk@aitech.ac.jp>
2020-12-10 01:19:18 +0900

    ファイルアップロード確認しました

--- group6_11/web.py
+++ group6_11/web.py
@@ -1,41 +1,56 @@
 import os
 # request フォームから送信した情報を扱うためのモジュール
-# url_for アドレス遷移
 from flask import Flask, request, render_template, send_from_directory
 # ファイル名をチェックする関数
 from werkzeug.utils import secure_filename
 import glob
 
-# 画像のアップロード先のディレクトリ
-UPLOAD_FOLDER = './uploads'
-# アップロードされる拡張子の制限
-ALLOWED_EXTENSIONS = set(['png', 'jpg'])
-
 app = Flask(__name__)
 
-app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+# 画像のアップロード先のディレクトリ
+UPLOAD_FOLDER = './input-image'
 
-# http://127.0.0.1:5000/
-@app.route('/',methods=['GET', 'POST'])
-def uploads_file():
-    # リクエストがポストかどうかの判別
-    if request.method == 'POST':
-        # ファイルがなかった場合の処理
-        if 'file' not in request.files:
-            flash('ファイルがありません')
-            return render_template(request.url)
-        # データの取り出し
-        file = request.files['file']
-        # ファイルのチェック
-        if file and allwed_file(file.filename):
-            # 危険な文字を削除（サニタイズ処理）
-            filename = secure_filename(file.filename)
-            # ファイルの保存
-            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-            # アップロード後のページに転送
-            return render_template(url_for('uploaded_file', filename=filename))
+
+OUTPUT_FACEWAKU_FOLDER = './facewaku'
+OUTPUT_GRAYSCALE_FOLDER = './grayscale-image'
+
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.config['OUTPUT_FACEWAKU_FOLDER'] = OUTPUT_FACEWAKU_FOLDER
+app.config['OUTPUT_GRAYSCALE_FOLDER'] = OUTPUT_GRAYSCALE_FOLDER
+
+
+
+@app.route('/')
+def index():
     return render_template("index.html")
 
+
+@app.route('/upload',methods=['POST'])
+def uploads_file():
+    if 'file' not in request.files:
+        render_template("index.html",message="ファイルを選択してください")
+
+    # データの取り出し
+    file = request.files['file']
+
+    filename = file.filename
+
+    if filename == '':
+        render_template("index.html",message="ファイルを選択してください")
+    
+    # 下記のような情報がFileStorageからは取れる
+    print('file_name={}'.format(filename))
+    print('content_type={} content_length={}, mimetype={}, mimetype_params={}'.format(
+        file.content_type,
+        file.content_length,
+        file.mimetype,
+        file.mimetype_params))
+
+    # ファイルの保存
+    file.save(os.path.join(UPLOAD_FOLDER, filename))
+
+    return render_template("index.html",message="アップロードが完了しました")
+    
 if __name__ == "__main__":
     #　完成したら"debug=True"を消す
     app.run(debug=True)
